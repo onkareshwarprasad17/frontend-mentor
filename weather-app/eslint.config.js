733d5493eba73084f95eec5e6d0f214584a8c3cf
Onkareshwar Prasad <onkareshwarkupra@gmail.com>
2025-09-14 14:39:45 +0530

    feat(weather-app): prettier, eslint configuration

--- weather-app/eslint.config.js
+++ weather-app/eslint.config.js
@@ -4,6 +4,8 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
+import eslintPluginPrettier from 'eslint-plugin-prettier'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default tseslint.config([
   globalIgnores(['dist']),
@@ -12,9 +14,16 @@ export default tseslint.config([
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
+      eslintConfigPrettier, // Disables conflicting ESLint rules
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      'prettier/prettier': 'error', // Shows Prettier errors as ESLint errors
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
